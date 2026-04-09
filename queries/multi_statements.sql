81cd5c68c7a16a14e72ff1ae13b6a10b5005f253
이경근 <lsg008@naver.com>
2026-04-09 09:38:19 +0900

    마지막 발표 준비

--- queries/multi_statements.sql
+++ queries/multi_statements.sql
@@ -1,2 +1,2 @@
-INSERT INTO users VALUES (2, 'JunHee', 26, 350);
-INSERT INTO users VALUES (3, 'JeongHyeon', 19, 310);
+INSERT INTO users VALUES (1, 'JunHee', 26, 350);
+INSERT INTO users VALUES (3, 'JeongHyeon', 19, 290);
